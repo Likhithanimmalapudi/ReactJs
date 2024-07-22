d3c9a41f8788aa5059bf22283dd750a1630137c3
Likhitha Chowdary <likhithachowdary333@gmail.com>
2024-07-22 22:02:35 +0530

    TestCommit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 import One from "./One";
 const rootElement = document.getElementById('root');
 const rootRef = ReactDOM.createRoot(rootElement);
